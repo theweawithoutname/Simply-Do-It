b06ba8ebbbc982cd8d68e87d933473c16f568277
theweawithoutname <lucciano_c9@hotmail.com>
2025-05-09 20:27:05 -0400

    Agregar tarjeta

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
+import { useState } from "react";
 import "./global.css"
 import Header from "./components/header/header"
 import Card from "./components/Card"
 import Button from "./components/Button"
 import { Plus } from "lucide-react";
 
-
 function App() {
+  // Estado para almacenar la lista de Cards
+  const [cards, setCards] = useState([{ id: Date.now }]); // Inicia con una Card
 
+  // Función para agregar nueva Card
+  const addCard = () => {
+    const newId = Date.now(); // ID único
+    setCards([...cards, { id: newId }]);
+  };
 
   return (
     <div>
       <Header />
       <div className="flex flex-wrap justify-start-center">
-        <Card />
-        <Button className="mt-[30px]" startIcon={<Plus size={20} color="lime" />} />
+        {/* Mapear todas las Cards */}
+        {cards.map((card) => (<Card key={card.id} />))}
+        
+        {/* Botón para agregar nuevas Cards */}
+        <Button
+          className="mt-[30px]"
+          startIcon={<Plus size={20} color="lime" />}
+          onClick={addCard}
+        />
       </div>
-      
     </div>
-    
-  )
+  );
 }
 
 export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,14 @@
 // Button.jsx
-function Button({ startIcon, className }) {
+function Button({ startIcon, className, onClick, children }) {
     const baseClasses = "flex flex-wrap justify-center items-center m-[5px] size-[30px] bg-yellow-600 hover:bg-yellow-700 text-white font-bold rounded";
     const combinedClassName = `${baseClasses} ${className || ''}`;
   
     return (
-      <button className={combinedClassName}>
+      <button 
+        className={combinedClassName} 
+        onClick={onClick}>
         {startIcon}
+        {children}
       </button>
     );
   }
